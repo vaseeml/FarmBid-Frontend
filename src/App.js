d62537ed2321a79759ef8b581677871f33c682af
vaseeml <ladafv@gmail.com>
2024-04-05 17:52:38 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Link , Route , Routes , BrowserRouter} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Routes>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
